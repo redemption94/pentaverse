4f87450ccf6a9d9c60aa6ac50be5c93f9eed227c
redemption94 <mihalceaclaudiu94@gmail.com>
2026-01-03 12:37:53 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        penta: {
+          black: '#000000',
+          dark: '#0A0A0A',
+          yellow: '#F6C85E', // Galbenul strălucitor
+          gold: '#DE9B35',   // Aurul închis
+          border: '#1A1A1A'
+        }
+      }
+    }
+  }
+}
